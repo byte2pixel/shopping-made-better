ea2ae0e99ae366ff70a418031607aadaeb133777
Zack <zyweisheit@student.fullsail.edu>
2026-07-23 13:54:41 -0400

    Removed more unused imports

--- app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListItemComparisonScreen.kt
+++ app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListItemComparisonScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
@@ -34,7 +33,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
