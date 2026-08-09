05702225b6bf6c3522fd8639fd127f364b6a528b
Zack <zyweisheit@student.fullsail.edu>
2026-08-09 16:05:51 -0400

    Made shopping list details pane more polished

--- app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListItemsScreen.kt
+++ app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListItemsScreen.kt
@@ -9,16 +9,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -65,17 +67,21 @@ fun ShoppingListItemsScreen(
     }
 }
 @Composable
-fun ListRow(item : ShoppingListItems, viewModel: ShoppingListItemsViewModel)
-{
-    Row(Modifier.fillMaxWidth().border(1.dp, Color.Gray))
+fun ListRow(item : ShoppingListItems, viewModel: ShoppingListItemsViewModel) {
+
+    Card(Modifier.fillMaxWidth(), colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surfaceVariant))
     {
-        IconButton(onClick = {
-            viewModel.deleteItems(item.id)
-            viewModel.getItems(item.shoppingListId)})
+        Row(Modifier.fillMaxWidth().padding(12.dp), verticalAlignment = Alignment.CenterVertically)
         {
-            Icon(painterResource(id = R.drawable.ic_delete), contentDescription = "delete")
-        }
-        Text(item.title)
+            Text(item.title, Modifier.weight(1f), style = MaterialTheme.typography.bodyLarge)
+            IconButton(onClick = {
+                viewModel.deleteItems(item.id)
+                viewModel.getItems(item.shoppingListId)
+            })
+            {
+                Icon(painterResource(id = R.drawable.ic_delete), contentDescription = "delete")
+            }
+    }
     }
 }
 
