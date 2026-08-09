86b9388e7a02f1833d8e2c97a604f669896d8153
Zack <zyweisheit@student.fullsail.edu>
2026-08-09 14:01:42 -0400

    Fixed Warnings

--- app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/domain/ShoppingListUseCase.kt
+++ app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/domain/ShoppingListUseCase.kt
@@ -1,8 +1,6 @@
 package com.fullsail.shoppingmadebetter.feature.shoppinglists.domain
 
 import com.fullsail.shoppingmadebetter.core.domain.UseCase
-import com.fullsail.shoppingmadebetter.feature.shoppinglists.data.ShoppingListDto
-
 interface ShoppingListUseCase: UseCase<ShoppingList, ShoppingListUseCase.Output> {
     sealed interface Output {
         data class Success(val list : ShoppingList) : Output
