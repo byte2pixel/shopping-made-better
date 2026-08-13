6f3dafb2012174a10b7a49cbb11bf32bdf471b2a
taffy <165635843+sour-taffy@users.noreply.github.com>
2026-08-13 08:09:45 -0400

    Changed buttons to icons where applicable and switched to a better looking alert dialogue and removed warnings. (#79)

--- app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListItemComparisonScreen.kt
+++ app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListItemComparisonScreen.kt
@@ -1,7 +1,6 @@
 package com.fullsail.shoppingmadebetter.feature.shoppinglists.ui
 
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,15 +12,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
@@ -29,6 +31,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -39,12 +42,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.fullsail.shoppingmadebetter.R
 import com.fullsail.shoppingmadebetter.feature.shoppinglists.domain.insertItem.InsertItem
 import com.fullsail.shoppingmadebetter.feature.shoppinglists.domain.shoppingTrip.ShoppingTrip
 import com.fullsail.shoppingmadebetter.feature.shoppinglists.domain.storeProductPricing.StoreProductPricing
@@ -138,15 +142,19 @@ fun ShoppingListItemComparisonScreen(
 
         if (pickList)
         {
-            BasicAlertDialog(onDismissRequest = {}, Modifier.fillMaxWidth(), DialogProperties(),
-                {
-                    Column(modifier = Modifier.fillMaxWidth().background( color = MaterialTheme.colorScheme.surface).padding(24.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-                        val lists by viewModel.shoppingLists.collectAsState()
-                        for (s in lists)
+
+            AlertDialog(
+                onDismissRequest = {},
+                title = { Text(text = "Select list:") },
+                text = {
+                    Column()
+                    {
+                    val lists by viewModel.shoppingLists.collectAsState()
+                    for (s in lists)
+                    {
+                        if (s.storeId == product.storeId)
                         {
-                            if (s.storeId == product.storeId)
-                            {
-                            Button(onClick = {
+                            Button(modifier = Modifier.fillMaxWidth(), onClick = {
                                 list = storeList.firstOrNull {it.shoppingListId  == s.shoppingListId }
                                 onAddClicked()
                             }
@@ -156,18 +164,20 @@ fun ShoppingListItemComparisonScreen(
                             }
 
                         }
-                        }
-                        Button(onClick = {
+                    }
+                    Button(modifier = Modifier.fillMaxWidth(), onClick = {
                         onAddNewListClicked()
                         pickList = false
                     })
                     {
-                       Text("Create new list")
+                        Text("Create new list")
                     }
-
-
                     }
-                })
+                },
+                confirmButton = {
+                },
+                )
+
 
 
 
@@ -175,26 +185,23 @@ fun ShoppingListItemComparisonScreen(
 
         if (showDialog)
         {
-               BasicAlertDialog(onDismissRequest = {}, Modifier.fillMaxWidth(), DialogProperties(),
-                {
-                    Column(modifier = Modifier.fillMaxWidth().background( color = MaterialTheme.colorScheme.surface).padding(24.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-
+            AlertDialog(
+                onDismissRequest = {},
+                title = { Text(text = "Enter list title:") },
+                text = {
                     OutlinedTextField(modifier = Modifier.fillMaxWidth(),value = listName, onValueChange = {listName = it},label= { Text("Enter list name") })
                     Spacer(modifier = Modifier.height(50.dp))
-                    Button(enabled= listName.isNotBlank(),
-                            onClick = {
-                                showDialog = false
+
+                },
+                confirmButton = {
+                    TextButton(onClick = {  showDialog = false
                         viewModel.createListAddItem(product,listName)
-                        onItemComparison()
+                        onItemComparison() }) {
+                        Text(text = "Ok")
                     }
-                        )
-                    {
-                        Text("Ok")
-                    }
+                },
 
-                    }
-            })
-
+            )
         }
 
 
@@ -225,24 +232,24 @@ fun ShoppingListItemComparisonScreen(
                 Row(
                 Modifier.align(Alignment.End)
                 ){
-                    Button(
+                    IconButton(
                         onClick =
                             {
                                pickList = true
                             },
                     )
                     {
-                        Text("Add")
+                        Icon(painterResource(id = R.drawable.ic_shopping_cart), contentDescription = "Add to existing cart", Modifier.size(24.dp))
                     }
 
-                    Button(
+                    IconButton(
                         onClick =
                             {
                                 onAddNewListClicked()
                             },
                     )
                     {
-                        Text("Add in New List")
+                        Icon(painterResource(id = R.drawable.ic_add_shopping_cart), contentDescription = "add to new cart", Modifier.size(24.dp))
                     }
                 }
             }
