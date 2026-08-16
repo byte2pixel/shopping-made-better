85c03eb7a3c27a56261364d78212eecd45f615bb
taffy <165635843+sour-taffy@users.noreply.github.com>
2026-08-16 13:08:50 -0400

    Scrum 171 (#82)
    * Ability to open lists in shopping / cart mode
    
    * Fixed some warnings
    
    * Update MainActivity.kt

--- app/src/main/java/com/fullsail/shoppingmadebetter/MainActivity.kt
+++ app/src/main/java/com/fullsail/shoppingmadebetter/MainActivity.kt
@@ -67,6 +67,7 @@ import kotlinx.coroutines.launch
 import com.fullsail.shoppingmadebetter.feature.onboarding.ui.OnboardingScreen
 import com.fullsail.shoppingmadebetter.feature.profile.ui.ChangePasswordScreen
 import com.fullsail.shoppingmadebetter.feature.profile.ui.ProfileScreen
+import com.fullsail.shoppingmadebetter.feature.shoppinglists.ui.ShoppingListCartScreen
 import com.fullsail.shoppingmadebetter.feature.shoppinglists.ui.ShoppingListItemsScreen
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -223,6 +224,10 @@ fun ShoppingMadeBetterApp(
                 ShoppingListsScreen(onItemComparison = {
                     navController.navigate(it)
                 })
+            }
+
+            composable<Dest.ShoppingListCartScreen>{
+                ShoppingListCartScreen( listId = it.toRoute<Dest.ShoppingListCartScreen>().listId )
             }
             composable<Dest.ShoppingListItemsScreen>{
                 ShoppingListItemsScreen( listId = it.toRoute<Dest.ShoppingListItemsScreen>().listId )
---
+++ app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListCartScreen.kt
@@ -0,0 +1,96 @@
+package com.fullsail.shoppingmadebetter.feature.shoppinglists.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.fullsail.shoppingmadebetter.feature.shoppinglists.domain.ShoppingListItems
+
+@Composable
+fun ShoppingListCartScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ShoppingListItemsViewModel = hiltViewModel(),
+    listId : String
+
+) {
+
+    val uiState by viewModel.uiState.collectAsState()
+
+
+
+    Box() {
+       LaunchedEffect(listId) { viewModel.getItems(listId)}
+        when (val state = uiState) {
+            ShoppingTripsUiState.Loading ->
+                CircularProgressIndicator(Modifier.align(Alignment.Center))
+
+            ShoppingTripsUiState.Error ->
+                Text("Couldn't load your lists", Modifier.align(Alignment.Center))
+
+            is ShoppingListItemsState.Success ->
+                if (state.items.isEmpty()) {
+                    Text("No items added to list", Modifier.align(Alignment.Center))
+                } else {
+                    LazyColumn(
+                        Modifier.fillMaxSize().padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(12.dp),
+                    ) {
+
+                        items(state.items, key = { it.id }) { CartRow(it,viewModel) }
+                    }
+                }
+
+
+            else -> {}
+        }
+        FloatingActionButton(onClick = {}, Modifier.fillMaxWidth().align(Alignment.BottomCenter).padding( 20.dp)) { Text("Complete List")}
+    }
+}
+@Composable
+fun CartRow(item : ShoppingListItems, viewModel: ShoppingListItemsViewModel) {
+
+    var clicked by remember { mutableStateOf(false)}
+    Card(Modifier.fillMaxWidth().clickable(onClick = {
+        clicked = !clicked
+
+    }), colors = CardDefaults.cardColors(
+
+        if (!clicked)
+        {
+            MaterialTheme.colorScheme.surfaceVariant
+        } else {
+            MaterialTheme.colorScheme.error
+        }
+    ))
+    {
+
+        Row(Modifier.fillMaxWidth().padding(4.dp), verticalAlignment = Alignment.CenterVertically)
+        {
+            Text(item.title, Modifier.weight(1f), style = MaterialTheme.typography.bodyLarge)
+        }
+    }
+}
+
+
