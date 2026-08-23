979af18fa21736d587026a69a89e8d7c0e5e1fd4
Zack <zyweisheit@student.fullsail.edu>
2026-08-23 13:58:08 -0400

    Fixes migration issues (hopefully)

--- supabase/migrations/20260820005235_shopping_list_item_update.sql
+++ supabase/migrations/20260820005235_shopping_list_item_update.sql
@@ -16,4 +16,14 @@ WITH CHECK (
         FROM public.shopping_lists sl
         WHERE sl.id = shopping_list_items.shopping_list_id
         AND sl.user_id = auth.uid()
-))
+));
+
+CREATE OR REPLACE view public.shopping_list_item_named_view with (security_invoker = on) as
+ SELECT li.id,
+    li.shopping_list_id AS shopping_id,
+    li.product_id,
+    li.quantity::integer AS quantity,
+    p.title,
+    li.is_checked
+   FROM shopping_list_items li
+     JOIN products p ON p.id = li.product_id;
