031973d3c0d81066e69b4c3e001b9463d37bd988
Zack <zyweisheit@student.fullsail.edu>
2026-08-27 18:17:26 -0400

    Removed unused function used for testing

--- app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListItemsViewModel.kt
+++ app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListItemsViewModel.kt
@@ -67,21 +67,6 @@ class ShoppingListItemsViewModel @Inject constructor(
         }
     }
 
-    fun checkItem(id : String, state : Boolean){
-        viewModelScope.launch{
-            when (val out = getIsCheckedUseCase.execute(isChecked(id, state))) {
-                is isCheckedUseCase.Output.Success ->
-                {
-
-                }
-                is isCheckedUseCase.Output.Failure ->
-                    ShoppingListItemsState.Error
-
-
-            }
-        }
-    }
-
     fun deleteItems(input : String, listId : String? = null)
     {
         _uiState.value = ShoppingListItemsState.Loading
