9e7c9091d45a5aea5480ab9e6235b9bac392e6d1
Jourdyn <Khalidrazai90@gmail.com>
2026-09-02 00:01:25 -0400

    SCRUM-225: Implement Share Recipe Action (#141)

--- app/src/main/java/com/fullsail/shoppingmadebetter/feature/meals/ui/MealDetailsScreen.kt
+++ app/src/main/java/com/fullsail/shoppingmadebetter/feature/meals/ui/MealDetailsScreen.kt
@@ -23,7 +23,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil3.compose.AsyncImage
 import com.fullsail.shoppingmadebetter.ui.theme.ShoppingMadeBetterTheme
 
