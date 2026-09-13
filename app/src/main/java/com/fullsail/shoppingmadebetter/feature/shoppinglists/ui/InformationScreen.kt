0b1a27041a6ea2de453ad7fc6e0a29316b8a45f7
taffy <165635843+sour-taffy@users.noreply.github.com>
2026-09-13 01:59:36 -0400

    SCRUM-269 Built out information screen (#175)
    * Built out information screen
    
    * Update InformationScreen.kt
    
    * fixed

--- app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/InformationScreen.kt
+++ app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/InformationScreen.kt
@@ -1,8 +1,16 @@
 package com.fullsail.shoppingmadebetter.feature.shoppinglists.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,7 +18,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import coil3.compose.AsyncImage
+import com.fullsail.shoppingmadebetter.core.ui.ProductImage
 
 @Composable
 fun InformationScreen(
@@ -29,7 +40,21 @@ fun InformationScreen(
                 Text("Couldn't load your lists", Modifier.align(Alignment.Center))
 
             is ItemInformationState.Success ->{
-                state.item.id.description?.let { Text(it) }
+                Column()
+                {
+
+                    ProductImage(imageUrl = state.item.id.image, modifier = Modifier.height(100.dp).fillMaxWidth(),  contentDescription = state.item.id.title)
+                    state.item.id.title?.let { Text(it, style = MaterialTheme.typography.headlineLarge) }
+                   Row()
+                   {
+                       state.item.id.brand?.let { Text(it,style = MaterialTheme.typography.labelLarge) }
+                       Spacer(modifier = Modifier.padding(12.dp))
+                       state.item.id.lifeCategory?.let { Text(it,style = MaterialTheme.typography.labelLarge) }
+                   }
+                    HorizontalDivider()
+                    state.item.id.description?.let { Text(it,style = MaterialTheme.typography.bodyLarge) }
+                }
+
             }
 
 
