578e8791825312548c0c883f1748c081847ac486
taffy <165635843+sour-taffy@users.noreply.github.com>
2026-09-13 14:04:38 -0400

    Removed icon off of lists (#188)
    * Removed icon off of lists
    
    * Update ShoppingListScreen.kt

--- app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListScreen.kt
+++ app/src/main/java/com/fullsail/shoppingmadebetter/feature/shoppinglists/ui/ShoppingListScreen.kt
@@ -207,11 +207,6 @@ private fun TripCard(trip: ShoppingTrip, onDelete: () -> Unit, onItemComparison 
                 {
                     Icon(painterResource(id = R.drawable.ic_delete), contentDescription = "delete", Modifier.size(24.dp))
                 }
-                IconButton(onClick = {onItemComparison(Dest.ShoppingListItemsScreen(trip.shoppingListId))})
-                {
-                    Icon(painterResource(id = R.drawable.ic_shopping_lists), contentDescription = "details", Modifier.size(24.dp))
-
-                }
                 IconButton(onClick = {onItemComparison(Dest.ShoppingListCartScreen(trip.shoppingListId))})
                 {
                     Icon(painterResource(id = R.drawable.ic_cart), contentDescription = "Cart", Modifier.size(24.dp))
